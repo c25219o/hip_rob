eeb1f527de972372a7a1b1da7f5eb331c07b972f
ishino <ishino.y@opst.co.jp>
2016-06-17 17:34:18 +0900

    ユーティリティ修正

--- HipRobberGame/src/shiri_tori/StringUtil.java
+++ HipRobberGame/src/shiri_tori/StringUtil.java
@@ -1,19 +1,13 @@
 package shiri_tori;
 
 public class StringUtil {
-	
+
 	public static char getLastChar(String word) {
 		return word.charAt(word.length() - 1);
 	}
-	
-
 
 	public static boolean isEmpty(String inputWord) {
-		if ("".equals(inputWord) || null == inputWord) {
-			return true;
-		} else {
-			return false;
-		}
+		return "".equals(inputWord) || null == inputWord;
 	}
 
 }
